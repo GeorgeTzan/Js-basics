473b1631911852ff49124a6cb483cc80ecfb7636
Giwrgos Tzanopoulos <giwrgos.tzanopoulos@gmail.com>
2024-07-03 21:43:38 +0300

    Added ex001

---
+++ ex001/index.js
@@ -0,0 +1,22 @@
+
+function Add(first, second){
+    var sum = parseFloat(first) + parseFloat(second);
+    alert("The sum is: " + sum);
+}
+
+let person = {
+    name: 'Giwrgos',
+    age: 19,
+    University: 'UOI',
+};
+
+function PersonInf(){
+    let i = 0;
+    alert(person.name + "\n" + person.age + "\n" + person.University);
+    
+}
+function Sub(first, second){
+    var sub = parseFloat(first) - parseFloat(second);
+    alert("The Subtraction is: " + sub);
+    
+}
