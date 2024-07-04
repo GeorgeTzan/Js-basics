77aee3142b072b779e93ee497910aac1d5dab8d9
Giwrgos Tzanopoulos <giwrgos.tzanopoulos@gmail.com>
2024-07-04 17:42:33 +0300

    Code readability

--- ex002/index.js
+++ ex002/index.js
@@ -1,8 +1,7 @@
 let first_choice, second_choice;
 let selected_operator;
 
-
-function assignValues(value) {
+function assign_Values(value) {
     if (first_choice === undefined) {
         first_choice = value;
     
@@ -12,41 +11,45 @@ function assignValues(value) {
     
     else if (second_choice === undefined){
         second_choice = value;
-        
+
     }else if( second_choice !== undefined){
         second_choice = Number(String(second_choice) + String(value));
     }
-    console.log(first_choice, second_choice);
 }
-function assignOperator(value) {
+
+function assign_Operator(value) {
     selected_operator = value;
 }
 
-function Operator(){
+function Oprator_Switch(){
+    switch (selected_operator) {
+        case '+':
+            final_choice = parseFloat(first_choice) + parseFloat(second_choice);
+            break;
+        case '-':
+            final_choice = parseFloat(first_choice) - parseFloat(second_choice);
+            break;
+        case '*':
+            final_choice = parseFloat(first_choice) * parseFloat(second_choice);
+            break;
+        case '/':
+            if (second_choice != 0){
+                final_choice = parseFloat(first_choice) / parseFloat(second_choice);
+                
+            }else{
+                alert("Can't Divide by zero");
+            }
+            break;
+    
+        default:
+            break;
+    }
+}
+
+function Operator_result(){
     if (first_choice !== undefined && second_choice !== undefined){
         let final_choice;
-        switch (selected_operator) {
-            case '+':
-                final_choice = parseFloat(first_choice) + parseFloat(second_choice);
-                break;
-            case '-':
-                final_choice = parseFloat(first_choice) - parseFloat(second_choice);
-                break;
-            case '*':
-                final_choice = parseFloat(first_choice) * parseFloat(second_choice);
-                break;
-            case '/':
-                if (first_choice != 0){
-                    final_choice = parseFloat(first_choice) / parseFloat(second_choice);
-                    
-                }else{
-                    alert("Can't Divide by zero");
-                }
-                break;
-        
-            default:
-                break;
-        }
+        OpratorSwitch()
         alert('Your result is:' + final_choice);
         first_choice = undefined;
         second_choice = undefined;
